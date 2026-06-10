f6466637bbfd41d520253130bddf24d60c9bb1e4
kgsharma7417 <krishangopalsh2424@gmail.com>
2026-06-10 15:10:12 +0530

    fix: vite to dependencies

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
-
-export default {
   server: {
-    host: true  // network par accessible ho jaye
-  }
-}
+    host: true,
+  },
+});
